48e4ef494f22b8461cea75a27cd9179f20ec62e1
Huseyn Gasimov <gasimov.huseyn@gmail.com>
2019-08-21 16:17:41 +0200

    Uninstall script for windows

---
+++ uninstall.bat
@@ -0,0 +1,29 @@
+@echo off
+
+where docker > nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo Please make sure that docker is running before continuing
+    pause
+)
+
+REM delete config folder
+DEL /F/Q/S config > NUL
+RMDIR /Q/S config
+
+docker volume rm intelecai_db-data
+docker volume rm intelecai_deployed-models
+docker volume rm intelecai_sftp-ssh
+docker volume rm intelecai_training
+docker volume rm intelecai_user-home
+docker volume rm intelecai_config-db
+docker volume rm intelecai_config-nginx
+docker volume rm intelecai_config-sftp 
+
+docker image rm intelecai/automl-server
+docker image rm intelecai/inference-server
+
+echo Intelec AI was uninstalled successfully.
+
+echo.
+echo Press any key to exit . . .
+pause > nul
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+hash docker 2>/dev/null || {
+    echo "Please make sure that docker is running before continuing "
+    read -n 1 -s -r -p "Press any key to continue ..."
+    echo 
+}
+
 rm -rf config
 
 docker volume rm intelecai_db-data
@@ -10,3 +16,5 @@ docker volume rm intelecai_user-home
 
 docker image rm intelecai/automl-server
 docker image rm intelecai/inference-server
+
+echo "Intelec AI was uninstalled successfully."
