caff61723da880a62c6383163ab54aac18daf9a9
Huseyn Gasimov <gasimov.huseyn@gmail.com>
2019-12-25 21:16:31 +0100

    start_servers.sh/bat to show users downloading images

--- start_servers.bat
+++ start_servers.bat
@@ -6,4 +6,13 @@ IF %ERRORLEVEL% NEQ 0 (
     pause
 )
 
+docker run --rm intelecai/automl-server echo 'AutoML Server image is ready'
+docker run --rm intelecai/inference-server echo 'Inference Server image is ready'
+docker run --rm mysql:5.7.24 echo 'MySQL Server image is ready'
+docker run --rm nginx:1.15-alpine echo 'Nginx Server image is ready'
+
 docker stack deploy -c docker-compose-windows.yml intelecai
+
+echo Starting Intelec AI ...
+ping 127.0.0.1 -n 11 > nul
+echo Please open http://localhost:7700 in your web browser to visit Intelec AI web page.
--- start_servers.sh
+++ start_servers.sh
@@ -6,8 +6,18 @@ hash docker 2>/dev/null || {
     echo
 }
 
+docker run --rm intelecai/inference-server echo 'Inference Server image is ready'
+docker run --rm mysql:5.7.24 echo 'MySQL Server image is ready'
+docker run --rm nginx:1.15-alpine echo 'Nginx Server image is ready'
+
 if [ "$1" = "--gpu" ]; then
+    docker run --rm intelecai/automl-server:latest-gpu echo 'AutoML Server (w/ GPU) image is ready'
     docker stack deploy -c docker-compose-gpu.yml intelecai
 else
+    docker run --rm intelecai/automl-server echo 'AutoML Server image is ready'
     docker stack deploy -c docker-compose.yml intelecai
 fi
+
+echo "Starting Intelec AI ..."
+sleep 10
+echo "Please open http://localhost:7700 in your web browser to visit Intelec AI web page."
